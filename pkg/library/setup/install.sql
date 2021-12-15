4474f4c52e86fbc7e98cc3de91cdd98cf7078bf6
Hank Shen <swh@admpub.com>
2021-12-15 11:40:47 +0800

    update

--- pkg/library/setup/install.sql
+++ pkg/library/setup/install.sql
@@ -84,4 +84,4 @@ CREATE TABLE `nging_forever_process` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-14 13:32:58
+-- Dump completed on 2021-12-15 11:40:12
