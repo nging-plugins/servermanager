3f66e110fd65854f2bc72470ea2eaba3a99800e0
Hank Shen <swh@admpub.com>
2021-12-15 13:52:36 +0800

    update

--- init.go
+++ init.go
@@ -1,9 +1,12 @@
 package servermanager
 
 import (
+	"github.com/nging-plugins/servermanager/pkg/handler"
 	_ "github.com/nging-plugins/servermanager/pkg/library/cmder"
 	_ "github.com/nging-plugins/servermanager/pkg/library/setup"
 )
+
+var LeftNavigate = handler.LeftNavigate
 
 func init() {
 
