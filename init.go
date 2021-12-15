4287cedc1a85e65ea2f10f40a0660442909fc3d8
Hank Shen <swh@admpub.com>
2021-12-15 10:48:39 +0800

    update

---
+++ init.go
@@ -0,0 +1,10 @@
+package servermanager
+
+import (
+	_ "github.com/nging-plugins/servermanager/pkg/library/cmder"
+	_ "github.com/nging-plugins/servermanager/pkg/library/setup"
+)
+
+func init() {
+
+}
