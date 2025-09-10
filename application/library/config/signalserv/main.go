f441cc2e414ff0a05fb74f76b3cde13299a38791
Hank Shen <swhbox@foxmail.com>
2025-09-10 20:47:30 +0800

    update

--- application/library/config/signalserv/main.go
+++ application/library/config/signalserv/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -12,7 +13,7 @@ func main() {
 	signal.Notify(signalChan)
 	go func() {
 		for sig := range signalChan {
-			fmt.Printf("收到信号: %v\n", sig)
+			fmt.Printf("[%s] 收到信号: %v\n", time.Now().Format(time.DateTime), sig)
 			// 如果需要特定处理，可以在这里添加逻辑
 			if sig == syscall.SIGINT || sig == syscall.SIGTERM {
 				fmt.Println("优雅退出...")
@@ -21,6 +22,6 @@ func main() {
 		}
 	}()
 
-	fmt.Println("程序运行中，按 Ctrl+C 退出...")
+	fmt.Println("[" + time.Now().Format(time.DateTime) + "] 程序运行中，按 Ctrl+C 退出...")
 	select {} // 阻塞主线程
 }
