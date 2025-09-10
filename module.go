91028a97c2e4d272193d46ff4bde4252f29c6ca2
Hank Shen <swhbox@foxmail.com>
2025-09-10 19:22:06 +0800

    update

--- module.go
+++ module.go
@@ -1,22 +1,18 @@
 package servermanager
 
 import (
-	//"github.com/coscms/webcore/library/config/cmder"
 	"github.com/coscms/webcore/library/cron"
 	"github.com/coscms/webcore/library/module"
 
 	"github.com/nging-plugins/servermanager/application/handler"
-	//pluginCmder "github.com/nging-plugins/servermanager/application/library/cmder"
 	"github.com/nging-plugins/servermanager/application/library/setup"
 )
 
 const ID = `server`
 
 var Module = module.Module{
-	//Startup: `daemon`,
-	// Cmder: map[string]cmder.Cmder{
-	// 	`daemon`: pluginCmder.New(),
-	// },
+	// Startup: ``,
+	// Cmder: map[string]cmder.Cmder{},
 	TemplatePath: map[string]string{
 		ID: `servermanager/template/backend`,
 	},
