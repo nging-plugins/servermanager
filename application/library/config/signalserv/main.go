a48ac08a3910842bd66a4c3de51790c1e0b999ee
Hank Shen <swhbox@foxmail.com>
2025-09-10 20:35:42 +0800

    update

---
+++ application/library/config/signalserv/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan)
+	go func() {
+		for sig := range signalChan {
+			fmt.Printf("收到信号: %v\n", sig)
+			// 如果需要特定处理，可以在这里添加逻辑
+			if sig == syscall.SIGINT || sig == syscall.SIGTERM {
+				fmt.Println("优雅退出...")
+				os.Exit(0)
+			}
+		}
+	}()
+
+	fmt.Println("程序运行中，按 Ctrl+C 退出...")
+	select {} // 阻塞主线程
+}
